23cde00708f44c6fdd0bb5cb69fb5dcf57cc5beb
Marzena Kaa <kacprowicz.marzena@gmail.com>
2020-03-14 19:57:17 +0100

    Initial commit

---
+++ src/main/java/pl/maakaa/SimpleKafkaStreamsProject/SimpleKafkaStreamsProjectApplication.java
@@ -0,0 +1,13 @@
+package pl.maakaa.SimpleKafkaStreamsProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleKafkaStreamsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleKafkaStreamsProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/maakaa/SimpleKafkaStreamsProject/SimpleKafkaStreamsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.maakaa.SimpleKafkaStreamsProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleKafkaStreamsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
