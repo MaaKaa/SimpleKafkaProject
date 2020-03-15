30c2db5997a7c00bdaebc6df8a8fabfaf95f57dc
Marzena Kaa <kacprowicz.marzena@gmail.com>
2020-03-15 12:40:36 +0100

    Create an interface defining Kafka streams.

---
+++ src/main/java/pl/maakaa/SimpleKafkaStreamsProject/MessagesStreams.java
@@ -0,0 +1,26 @@
+package pl.maakaa.SimpleKafkaStreamsProject;
+
+import org.springframework.cloud.stream.annotation.Input;
+import org.springframework.cloud.stream.annotation.Output;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.SubscribableChannel;
+
+/**
+ *  Interface that defines kafka streams.
+ */
+public interface MessagesStreams {
+    String INPUT = "greetings-in";
+    String OUTPUT = "greetings-out";
+
+    /**
+     * an inbound stream to read messages from a Kafka topic
+     */
+    @Input(INPUT)
+    SubscribableChannel inboundGreetings();
+
+    /**
+     * An outbound stream to write messages to a Kafka topic
+     */
+    @Output(OUTPUT)
+    MessageChannel outboundGreetings();
+}
