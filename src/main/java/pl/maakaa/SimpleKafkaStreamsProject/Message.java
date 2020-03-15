2cc223e0d6572709799e5a3594a75745d5f7c8c2
Marzena Kaa <kacprowicz.marzena@gmail.com>
2020-03-15 12:43:18 +0100

    Create message object - a model for the message to be sent.

---
+++ src/main/java/pl/maakaa/SimpleKafkaStreamsProject/Message.java
@@ -0,0 +1,18 @@
+package pl.maakaa.SimpleKafkaStreamsProject;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * A class that represents the message object that is read from and write to the kafka topic.
+ */
+@Getter
+@Setter
+@ToString
+@Builder
+public class Message {
+    private long timestamp;
+    private String content;
+}
