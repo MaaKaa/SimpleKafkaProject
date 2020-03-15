1c732cea0f428766fb5a89757f3cfe1e28a8534a
Marzena Kaa <kacprowicz.marzena@gmail.com>
2020-03-15 12:46:35 +0100

    Create a message listener - a consumer that will listen to messages on the Kafka topic and log them on the console.

---
+++ src/main/java/pl/maakaa/SimpleKafkaStreamsProject/MessagesListener.java
@@ -0,0 +1,22 @@
+package pl.maakaa.SimpleKafkaStreamsProject;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+/**
+ * Class listening to messages on the 'messages' kafka topic and logging them on the console.
+ */
+@Component
+@Slf4j
+public class MessagesListener {
+
+    /**
+     * Method that is invoked by Spring Cloud Stream with every new Message object on the 'messages' Kafka topic.
+     */
+    @StreamListener(MessagesStreams.INPUT)
+    public void handleMessage(@Payload Message message) {
+        log.info("Received message: {}", message);
+    }
+}
