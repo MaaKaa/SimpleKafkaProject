fec88c6f71f4602ac920951d8a9cf2652781e128
Marzena Kaa <kacprowicz.marzena@gmail.com>
2020-03-15 12:45:47 +0100

    Create a REST controller and a method defining an endpoint that takes a message request param and passes it to the sendMessage() method in MessagesService.

---
+++ src/main/java/pl/maakaa/SimpleKafkaStreamsProject/MessagesController.java
@@ -0,0 +1,34 @@
+package pl.maakaa.SimpleKafkaStreamsProject;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * REST API endpoint that triggers sending a message to kafka using the GreetingsService bean.
+ */
+@RestController
+public class MessagesController {
+    private final MessagesService messagesService;
+
+    public MessagesController(MessagesService messagesService) {
+        this.messagesService = messagesService;
+    }
+
+    /**
+     * Method defining an endpoint that takes a message request param and passes it to the sendMessage() method in MessagesService.
+     * @param message
+     */
+    @GetMapping("/messages")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public void messages(@RequestParam("message") String message) {
+        Message messages = Message.builder()
+                .content(message)
+                .timestamp(System.currentTimeMillis())
+                .build();
+
+        messagesService.sendMessage(messages);
+    }
+}
